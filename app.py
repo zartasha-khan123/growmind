e6917a3447641a5f5c2f0fe546c24c980a5a52c0
zartasha-khan123 <zartashimran12345@gmail.com>
2025-02-23 15:41:05 +0500

    d

---
+++ app.py
@@ -0,0 +1,64 @@
+# import streamlit as st
+
+# st.balloons()
+# st.balloons()
+# st.markdown(
+#     """
+#     <style>
+#     .stApp {
+#         background-color: #FFDDC1; /* Light peach background */
+#     }
+#     </style>
+#     """,
+#     unsafe_allow_html=True
+# )
+
+# st.title("Hello everyone, nice to meet you!")
+
+# for i in range(5):
+#     st.balloons()
+#     st.balloons()
+
+
+# slider1 = st.slider("Select first number", 100, 500)
+# slider2 = st.slider("Select second number", 100, 500)
+
+
+# import datetime
+
+# st.date_input("enter your date of birth",datetime.date(1990,1,1))
+
+
+
+
+
+
+
+# st.date_input("Enter your text")
+# st.time_input("Enter your text")
+# st.camera_input("Enter your text")
+# st.number_input("Enter your text")
+
+
+# st.sidebar.header("user input")
+
+# user_input=st.sidebar.slider('select your number',1 , 100,10)
+
+# st.write('this is title')
+
+# st.title("hello world")
+# st.header("this is a header")
+# st.markdown("# this is a markdown header")
+
+# st.code("""st.sidebar.header("user input")
+
+# user_input=st.sidebar.slider('select your number',1 , 100,10)
+
+# st.write('this is title')
+
+# st.title("hello world")
+# st.header("this is a header")
+# st.markdown("# this is a markdown header")""")
+
+
+
