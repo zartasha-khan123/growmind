6b7bc4d446ef5e9b66a7276d869d6210b956de83
zartasha-khan123 <zartashimran12345@gmail.com>
2025-03-05 02:40:03 +0500

    g

--- growmind.py
+++ growmind.py
@@ -19,7 +19,7 @@ if uploaded_files:
         if file_ext == ".csv":
             df = pd.read_csv(file)
         elif file_ext == ".xlsx":
-            df = pd.read_excel(file, engine="openpyxl")
+            df = pd.read_excel(file)
         else:
             st.error(f"Unsupported file type: {file_ext}")  # Error message for unsupported files
             continue
